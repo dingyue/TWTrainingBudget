d5fb181c645ee55151ac3fa15ed53351ece4813b
Ding Yue <yding@thoughtworks.com>
2012-12-09 15:46:55 +0800

    init a rails project.

---
+++ twtb/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twtb::Application.config.secret_token = 'e0d5c8af545397d470a419bf29b476577bf7f14dac488ceded1906e18aeededc64069cb2d036ba1e331eedcc469c5adf3c36f779785d62916c9c69b92c773e91'
